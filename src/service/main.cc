994d9eebaa12121f98b7c1cf277d4f3a857bbbd2
2323398255@qq.com <2323398255@qq.com>
2025-01-08 14:52:06 +0800

    first commit

---
+++ src/service/main.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+
+
+
+
+
+
+int main()
+{
+// 启动服务
+
+    return 0;
+}
---
+++ src/service/service.cc
@@ -0,0 +1,17 @@
+
+
+
+
+// 服务类
+class service
+{
+    public:
+    service();
+    ~service();
+    // 启动服务
+    void start();
+private:
+
+};
+
+
